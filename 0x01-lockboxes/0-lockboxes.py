d8997f58e406fa72a05b4f56f49e13e81c46e32e
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-12 22:23:41 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,15 @@
 def canUnlockAll(boxes):
-    # Initial setup: length of boxes and a list to track opened boxes
     boxes_length = len(boxes)
-    opened_boxes = [False] * boxes_length  # Track opened boxes
-    opened_boxes[0] = True  # The first box is unlocked
-    keys_queue = []  # This will keep track of which keys we can use
+    opened_boxes = [False] * boxes_length
+    opened_boxes[0] = True
+    keys_queue = []
 
-    # We begin with the keys in the first box
-    keys_queue.extend(boxes[0])  # Start with keys from the first box
+    keys_queue.extend(boxes[0])
 
-    while keys_queue:  # While there are keys to check
-        key = keys_queue.pop(0)  # Get the next key
-        if 0 <= key < boxes_length and not opened_boxes[key]:  # Check key validity
-            opened_boxes[key] = True  # Mark the box as opened
-            keys_queue.extend(boxes[key])  # Add the new keys from this box
+    while keys_queue:
+        key = keys_queue.pop(0)
+        if 0 <= key < boxes_length and not opened_boxes[key]:
+            opened_boxes[key] = True
+            keys_queue.extend(boxes[key])
 
-    # All boxes have been opened if all elements in opened_boxes are True
     return all(opened_boxes)
