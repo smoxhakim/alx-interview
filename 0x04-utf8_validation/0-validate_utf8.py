ae6a09cdbe822c12906cb71098c52da5397fb30e
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-31 09:20:46 +0100

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+def validUTF8(data):
+    """ UTF-8 Validation """
+
+    number_of_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for i in data:
+        mask_byte = 1 << 7
+
+        if number_of_bytes == 0:
+            while mask_byte & i:
+                number_of_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_of_bytes == 0:
+                continue
+
+            if number_of_bytes == 1 or number_of_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask1 and not (i & mask2)):
+                return False
+
+        number_of_bytes -= 1
+
+    if number_of_bytes == 0:
+        return True
+
+    return False
