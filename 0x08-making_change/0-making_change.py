7bf6adb0b68f4dcd195b6679c0939d0f0462ec1d
hakimsmox <abdelhakimsmox@gmail.com>
2024-11-28 17:12:19 +0100

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Making Change"""
+
+
+def makeChange(coins, total):
+    """
+        Making Change
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
